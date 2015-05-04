d833d50c897fc3e4298144dadfca4131a561071c
forest <forest@tom.com>
2015-05-05 02:45:30 +0800

    mm

--- udp/sudp.py
+++ udp/sudp.py
@@ -9,6 +9,7 @@ s.bind(('0.0.0.0', PORT))
 # s.bind(('127.0.0.1', PORT))
 print 'listening at', s.getsockname()
 while True:
+  # 每次最多获取64K的数据?
   data, address = s.recvfrom(MAX)
   print 'the client at', address, 'says', repr(data)
   s.sendto('your data was %d bytes' % len(data), address)
